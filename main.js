a252c4119a672674d72b54a9cb3ab5dd6daab5cb
FezVrasta <fez@tekhy.net>
2014-06-20 14:49:41 +0200

    Added Gist creation + code refactor

--- main.js
+++ main.js
@@ -4,11 +4,14 @@ define(function (require, exports, module) {
     "use strict";
 
     var panel                   = require("text!templates/panel.html"),
-        contentError              = require("text!templates/contentError.html"),
         content                 = require("text!templates/content.html"),
-        gist                    = require("text!templates/gist.html");
+        gist                    = require("text!templates/gist.html"),
+        newGistDialog           = require("text!templates/newGistDialog.html"),
+        successGistDialog       = require("text!templates/successGistDialog.html");
 
     var CommandManager          = brackets.getModule("command/CommandManager"),
+        DocumentManager         = brackets.getModule("document/DocumentManager"),
+        Dialogs                 = brackets.getModule("widgets/Dialogs"),
         EditorManager           = brackets.getModule("editor/EditorManager"),
         ExtensionUtils          = brackets.getModule("utils/ExtensionUtils"),
         Menus                   = brackets.getModule("command/Menus"),
@@ -20,10 +23,12 @@ define(function (require, exports, module) {
         $content                = $(),
         gists                   = null;
 
-    var TOGGLE_PANEL = "gist-manager.run";
+    var TOGGLE_PANEL  = "gist-manager.run",
+        GM_PANEL      = "gist-manager.panel",
+        NEW_GIST_MENU = "gist-manager.menu";
 
+    // Show or hide Gist Manager panel when called
     function _handlePanelToggle() {
-        $panel = $("#gist-manager");
 
         if ($panel.is(":visible")) {
             $panel.hide();
@@ -36,58 +41,61 @@ define(function (require, exports, module) {
         EditorManager.resizeEditor();
     }
 
-    CommandManager.register("Show Gist Manager", TOGGLE_PANEL, _handlePanelToggle);
-
+    // Render a given Gist inside the Gist Manager panel
     function renderGist(gistData) {
 
-        $panel = $("#gist-manager");
-
+        // If the gist we are trying to render is already loded
+        // don't load it again but just show the cached version
+        // if is not cached then load it from Gist API
         if (!$panel.find("#" + gistData.id).data("loaded")) {
 
+            // Load Gist data and prepare it for Mustache
             $.getJSON(gistData.url, function(gistData) {
 
+                // Convert .files from object to array (needed because Mustache is stupid I guess, or maybe I'm)
                 gistData.files = $.map(gistData.files, function(value) {
                     return [value];
                 });
 
+                // Render Gist using Mustache
                 var vars = gistData;
                 $.extend(vars, Strings);
                 var $gist = $(Mustache.render(gist, vars));
+
+                // Inject the rendered Gist in the Gist Manager panel
                 $panel.find("#" + gistData.id).html($gist).data("loaded", true);
-
             });
-
         }
 
+        // Move things around to show the selected Gist
         $panel
-        .find(".gist").hide().end()
-        .find("a").removeClass("active").end()
-        .find("a[href=#" + gistData.id + "]").addClass("active").end()
-        .find("#" + gistData.id).show();
-
+            .find(".gist").hide().end()
+            .find("a").removeClass("active").end()
+            .find("a[href=#" + gistData.id + "]").addClass("active").end()
+            .find("#" + gistData.id).show();
     }
 
+    // Load list of Gists
+    // if no username is provided then load the public list
+    // if username is provided then load the list of the selected username
+    // if password is provided then load even the secret Gists of the selected user
     function loadContent(username, password) {
 
+        // These will be used in our Ajax call
         var url,
             headers;
 
-        // Set headers and API URL depending if we are trying to get public gists, user's public gists or user's public&secret gists
+        // Set headers and API URL depending if we are trying to get public gists
+        // user's public gists or user's public & secret gists
         if (username.length && password.length) {
-
             url = "https://api.github.com/gists";
             headers = { "Authorization": "Basic " + btoa(username + ":" + password) };
-
         } else if (username.length) {
-
             url = "https://api.github.com/users/" + username + "/gists";
             headers = { };
-
         } else {
-
             url = "https://api.github.com/gists";
-            headers =  { };
-
+            headers = { };
         }
 
         $.ajax({
@@ -99,28 +107,23 @@ define(function (require, exports, module) {
                 gists = data;
                 _renderContent(gists);
             },
-            error: function () {
-                _renderContent([]);
+            error: function (err) {
+                Dialogs.showModalDialog("error-dialog", Strings.LOADING_ERROR, err.responseText.message);
             }
         });
 
+        // Renders a given list of Gists inside the panel
         function _renderContent(gists) {
 
-            if (gists.length > 0) {
-                var vars = {"gists": gists};
-                $content = $(Mustache.render(content, vars));
-            } else {
-                $content = $(Mustache.render(contentError, Strings));
-            }
-
+            var vars = {"gists": gists};
+            $content = $(Mustache.render(content, vars));
             $panel.find(".gist-manager-content").html($content);
 
             // Render the first gist
             renderGist(gists[0]);
 
-            $panel
-            .on("click", ".close", function() { CommandManager.execute(TOGGLE_PANEL); })
-            .on("click", ".list-group-item", function(event) {
+            // Add event handler on the list of Gists
+            $panel.on("click", ".list-group-item", function(event) {
 
                 gists.forEach( function(gist) {
                     if (gist.id == $(event.target).data("id")) {
@@ -133,20 +136,116 @@ define(function (require, exports, module) {
         }
     }
 
+    // Post a new Gist
+    function newGist(username, password) {
+
+        var content,
+            gistFileName = "",
+            filename = DocumentManager.getCurrentDocument().file._name,
+            selection = EditorManager.getCurrentFullEditor().getSelectedText();
+
+        content = (selection.length) ? selection : "";
+        if (selection.length && filename.length) {
+            gistFileName = filename;
+        }
+
+        var vars = $.extend({"content": content, "filename": gistFileName, "secret": (username.length && password.length)}, Strings);
+
+        var dialog  = Dialogs.showModalDialogUsingTemplate(Mustache.render(newGistDialog, vars)),
+            $dialog = dialog.getElement();
+
+            $dialog.
+                on("click", "#add-file", function() {
+                    $dialog.find("#prototype-file .file").first().clone().appendTo("#files");
+                });
+
+            dialog.done(function (buttonId) {
+                if (buttonId === "create-public-gist" || buttonId === "create-secret-gist") {
+
+                    var url = "https://api.github.com/gists",
+                        headers;
+
+                    var gistData = {
+                        "description": $dialog.find("[name=description]").val(),
+                        "public": (buttonId === "create-public-gist"),
+                        "files": { }
+                    };
+
+                    $dialog.find("#files .file").each( function() {
+                        gistData.files[$(this).find(".filename").val()] = {};
+                        gistData.files[$(this).find(".filename").val()].content = $(this).find(".content").val();
+                    });
+
+                    // Set header if user wants to be authenticated
+                    if (username.length && password.length) {
+                        headers = { "Authorization": "Basic " + btoa(username + ":" + password) };
+                    } else {
+                        headers =  { };
+                    }
+
+                    console.log(JSON.stringify(gistData));
+                    $.ajax({
+                        type: "POST",
+                        url: url,
+                        dataType: "json",
+                        headers: headers,
+                        data: JSON.stringify(gistData),
+                        success: function (response) {
+                            var vars = $.extend(response, Strings);
+                            var dialog = Dialogs.showModalDialogUsingTemplate(Mustache.render(successGistDialog, vars));
+
+                            dialog.done(function (buttonId) {
+                                if (buttonId === "open") {
+                                    brackets.app.openURLInDefaultBrowser(response.html_url);
+                                }
+                            });
+
+                            if (username.length && password.length) {
+                                loadContent(username, password);
+                            }
+                        },
+                        error: function (err) {
+                            Dialogs.showModalDialog("error-dialog", Strings.CREATION_ERROR, err.responseText.message);
+                        }
+                    });
+                }
+            });
+    }
+
     function init() {
 
+        // Load compiled CSS of Gist Manager
         ExtensionUtils.loadStyleSheet(module, "styles/gist-manager.css");
 
+        // Add menu option to toggle Gist Manager panel
+        CommandManager.register(Strings.SHOW_GIST_MANAGER, TOGGLE_PANEL, _handlePanelToggle);
         var menu = Menus.getMenu(Menus.AppMenuBar.VIEW_MENU);
-        menu.addMenuItem(TOGGLE_PANEL, null, Menus.AFTER);
+        menu.addMenuItem(TOGGLE_PANEL, null, Menus.BEFORE);
 
-        $panel = $(Mustache.render(panel, Strings));
-        PanelManager.createBottomPanel("fezvrasta.gist-manager.panel", $panel, 200);
+        // Add context menu option to create gist
+        CommandManager.register(
+            Strings.CREATE_NEW_GIST,
+            NEW_GIST_MENU,
+            function() { newGist($panel.find("#github-username").val(), $panel.find("#github-password").val()); }
+        );
+        var contextMenu = Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU);
+        contextMenu.addMenuItem(NEW_GIST_MENU);
 
-        $panel.on("click", "#load-gists", function() {
-            loadContent($panel.find("#github-username").val(), $panel.find("#github-password").val());
-        });
+        // Create Gist Manager panel
+        PanelManager.createBottomPanel(GM_PANEL, $(Mustache.render(panel, Strings)), 200);
 
+        // Cache selection of Gist Manager panel
+        $panel = $("#gist-manager");
+
+        // Add events handler to Gist Manager panel
+        $panel
+            .on("click", "#load-gists", function() {
+                loadContent($panel.find("#github-username").val(), $panel.find("#github-password").val());
+            })
+            .on("click", "#new-gist", function() {
+                newGist($panel.find("#github-username").val(), $panel.find("#github-password").val());
+            })
+            .on("click", ".close", _handlePanelToggle);
     }
 
     init();
--- nls/root/strings.js
+++ nls/root/strings.js
@@ -1,11 +1,24 @@
 /*jshint maxlen:false */
 
 define({
-    LOAD_GISTS:         "Load gists",
-    LOADING_ERROR:      "There was an error during the loading of Git Manager...",
-    USERNAME:           "username",
-    PASSWORD:           "password",
-    DOWNLOAD_GIST:      "Download Gist",
-    OPEN_IN_BROWSER:    "Open in browser"
+    SHOW_GIST_MANAGER:          "Show Gist Manager",
+    LOAD_GISTS:                 "Load gists",
+    NEW_GIST:                   "New gist",
+    USERNAME:                   "username",
+    PASSWORD:                   "password",
+    DOWNLOAD_GIST:              "Download Gist",
+    OPEN_IN_BROWSER:            "Open in browser",
+    CREATE_NEW_GIST:            "Create new Gist",
+    ADD_FILE:                   "Add file",
+    CANCEL:                     "Cancel",
+    CREATE_PUBLIC_GIST:         "Create public Gist",
+    CREATE_SECRET_GIST:         "Create secret Gist",
+    GIST_DESCRIPTION_HERE:      "Write your Gist description here...",
+    FILENAME_HERE:              "Write here an optional file name...",
+    GIST_CONTENT_HERE:          "Write your Gist content here...",
+    CREATION_ERROR:             "Failed to create Gist",
+    CREATION_SUCCESS:           "Gist successfully created",
+    CLOSE:                      "Close",
+    LOADING_ERROR:              "Loading error"
 
 });
